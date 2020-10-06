c3ca5d42d14a9f374f3fab7010f876f5f8d1c0bf
= <dadu3890@colorado.edu>
2020-10-05 18:58:38 -0600

    Added blocksize, hash in each block

--- blockchain.py
+++ blockchain.py
@@ -1,37 +1,37 @@
 import datetime as dt
-import hashlib
+from crypto import HashBlock
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
+        self.blockSize = 50
     
     def __len__(self):
         return len(self.chain)
     
     def create_genesis_block(self):
         return Block (
-            0, dt.datetime(2020, 1, 1), None, None, None
+            0, dt.datetime(2020, 1, 1), None, None
         )
     
     def add_block(self, data):
         idx  = len(self.chain)
         prev = self.chain[idx-1]
 
-        new_block = Block(
-            idx, str(dt.datetime.now()), prev, None, data #Update this to use the hash of prev as prev_block 
-        )
+        new_block = Block(idx, str(dt.datetime.now()), prev, data)
         self.chain.append(new_block)
     
     def nth_block(self, n):
         return self.chain[n] if n < len(self.chain) and n >=0 else None
 
-class Block:  
-    def __init__(self, idx, timestamp, prev_block, prev_hash, data):
-        self.idx = idx
-        self.timestamp = timestamp
+class Block:
+    def __init__(self, idx, timestamp, prev_block, data):
+        self.idx        = idx
+        self.timestamp  = timestamp
         self.prev_block = prev_block
-        self.prev_hash  = prev_hash
-        self.data = data
+        self.prev_hash  = prev_block.hash if prev_block is not None else None
+        self.data       = data
+        self.hash       = HashBlock(data)
     
     def __str__(self):
         prev = self.prev_block
