69a1af811b6d1d0d099c3e1fd7c394aa6171a4d7
= <dadu3890@colorado.edu>
2020-10-06 12:52:52 -0600

    Tests for head function and transaction parameters

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from blockchain import *
 chain = Blockchain()
 
 block1 = BlockData()
-block1.add_transaction(Transaction())
-block1.add_transaction(Transaction())
+t1 = Transaction(5, 'Me', 'You')
+block1.add_transaction(t1)
 print(block1)
 chain.add_block(block1)
 chain.add_block(BlockData())
@@ -13,4 +13,6 @@ chain.add_block(BlockData())
 print(chain.nth_block(0))
 print(chain.nth_block(1))
 print(chain.nth_block(2))
-print('Chain has {} block{}'.format(len(chain), 's' if len(chain) > 1 else ''))
+print('\nChain has {} block{}'.format(len(chain), 's' if len(chain) > 1 else ''))
+
+print('Head is block #{}'.format(chain.get_head().idx))
