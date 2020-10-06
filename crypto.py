7224bc4f230fb8c3326b953b1f131a0ff9ca1a66
= <dadu3890@colorado.edu>
2020-10-06 12:53:17 -0600

    Added HashTranscation method

--- crypto.py
+++ crypto.py
@@ -9,3 +9,16 @@ def HashBlock(block):
     
     stringified_block = ''.join(map(str, block.transactions))
     return Hash(stringified_block)
+
+def HashTransaction(transaction):
+    if transaction is None:
+        return None
+    
+    stringified_transaction = (
+        str(transaction.id) +
+        transaction.timestamp +
+        str(transaction.transfer_amount) +
+        transaction.sender +
+        transaction.recipient
+    )
+    return Hash(str(stringified_transaction))
