70bfb6ea329609f785992a6255c470d7ac88d511
= <dadu3890@colorado.edu>
2020-10-06 20:13:13 -0600

    Seperate hash functions for Block and BlockData

--- crypto.py
+++ crypto.py
@@ -1,17 +1,24 @@
 import hashlib
+import blockchain
 
 def Hash(data):
     return hashlib.sha256(data.encode()).hexdigest()
 
 def HashBlock(block):
-    if block is None:
+    if block is None or type(block) is not blockchain.Block:
         return None
     
-    stringified_block = ''.join(map(str, block.transactions))
+    return HashBlockData(block.data)
+
+def HashBlockData(block_data):
+    if block_data is None or type(block_data) is not blockchain.BlockData:
+        return None
+    
+    stringified_block = ''.join(map(str, block_data.transactions))
     return Hash(stringified_block)
 
 def HashTransaction(transaction):
-    if transaction is None:
+    if transaction is None or type(transaction) is not blockchain.Transaction:
         return None
     
     stringified_transaction = (
