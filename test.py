8088790325d7b4b5c4809b8dca18c4015f634947
= <dadu3890@colorado.edu>
2020-10-06 22:00:19 -0600

    Added test for block tampering

--- test.py
+++ test.py
@@ -3,16 +3,27 @@ from blockchain import *
 #Create a chain for testing
 chain = Blockchain()
 
-block1 = BlockData()
+#Create transactions
 t1 = Transaction(5, 'Me', 'You')
-block1.add_transaction(t1)
-print(block1)
-chain.add_block(block1)
-chain.add_block(BlockData())
+t2 = Transaction(100, 'You', 'Me')
 
+#Create and tamper with a block
+block1data = BlockData()
+block1data.add_transaction(t1)
+print(block1data)
+
+#Add the blocks, then tamper with the first
+chain.add_block(BlockData())
+chain.add_block(block1data)
+chain.add_block(BlockData())
+chain.nth_block(2).data = BlockData().add_transaction(t2)
+print("Block tampered?", chain.nth_block(3).prev_was_tampered())
+
+#Print the final state of each block
 print(chain.nth_block(0))
 print(chain.nth_block(1))
 print(chain.nth_block(2))
+print(chain.nth_block(3))
 print('\nChain has {} block{}'.format(len(chain), 's' if len(chain) > 1 else ''))
 
 print('Genesis is block #{}'.format(chain.get_genesis_block().idx))
