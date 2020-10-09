a6b99d77238ca868f5a90a452e07245ac84f641e
= <dadu3890@colorado.edu>
2020-10-09 06:24:16 -0600

    Proper tests of the get_balance function

--- browsercoin/tests/test_blockchain.py
+++ browsercoin/tests/test_blockchain.py
@@ -7,8 +7,9 @@ def test_blockchain():
     t_00 = Transaction(500, None, 'Me', None, None)
     t_01 = Transaction(500, None, 'You', None, None)
     t1 = Transaction(50, 'Me', 'You', t_00, None)
-    t2 = Transaction(300, 'You', 'Me', t_01, None)
-    t3 = Transaction(11, 'Me', 'You', t2, None)
+    t2 = Transaction(300, 'You', 'Me', t1, None)
+    t3 = Transaction(10, 'Me', 'You', t2, None)
+    t4 = Transaction(0, 'Me', 'You', t3, None)
 
     assert t1 == t1, 'Same transactions equal?'
     assert t1 != t2, 'Different transactions equal?'
@@ -22,24 +23,28 @@ def test_blockchain():
     block2data.add_transaction(t1)
     block2data.add_transaction(t2)
 
+    block3data = BlockData()
+    block3data.add_transaction(t3)
+
     #Add three blocks, then tamper with the second
     assert BlockData() != block1data, 'Different BlockDatas equal?'
     assert BlockData() == BlockData(), 'Same BlockDatas equal?'
 
     chain.add_block(block1data)
     chain.add_block(block2data)
-    chain.add_block(BlockData().add_transaction(t3))
+    chain.add_block(block3data)
 
     assert chain.nth_block(3).prev_was_tampered() == False, 'Unmodified block not tampered?'
-    chain.nth_block(2).data = BlockData().add_transaction(t2)
+    chain.nth_block(2).data = BlockData().add_transaction(t4)
     assert chain.nth_block(3).prev_was_tampered() == True, 'Modified block tampered?'
 
     assert chain.nth_block(1) != chain.nth_block(2), 'Different blocks equal?'
     assert chain.nth_block(1) == chain.nth_block(1), 'Same blocks equal?'
 
     #Check the balance of each address
-    assert chain.get_balance('Me') == 439, 'get_balance works?'
-    assert chain.get_balance('You') == 261, 'get_balance works?'
+    assert chain.get_balance('Me') == 740, 'get_balance works?'
+    assert chain.get_balance('You') == 260, 'get_balance works?'
+    assert chain.get_balance('Somebody else') == None, 'get_balance works?'
 
     #Print the final state of each block
     print(chain.nth_block(0))
