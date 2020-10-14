c1d07f57ef5fa42a27bac012c30da86c75ca2bf1
= <dadu3890@colorado.edu>
2020-10-14 13:41:13 -0600

    Added tests for coinbase transaction

--- browsercoin/tests/test_node.py
+++ browsercoin/tests/test_node.py
@@ -1,4 +1,14 @@
 from src.node import *
+from src.blockchain import *
+from src.crypto import *
 
 def test_node():
-    assert True
+    (testpub, testpriv) = rsa.newkeys(512)
+    chain = Blockchain()
+    master = MasterNode()
+    
+    data = BlockData()
+    master.add_coinbase(data, testpub)
+    chain.add_block(data)
+    assert chain.get_balance(testpub) == 50
+    assert chain.nth_block(1).is_valid()
