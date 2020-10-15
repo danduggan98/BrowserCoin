9818189acefc44eeba179250f66a3c5915c6259a
= <dadu3890@colorado.edu>
2020-10-14 18:33:06 -0600

    Reduced MAX_BLOCK_SIZE by one to allow room for coinbase

--- browsercoin/src/params.py
+++ browsercoin/src/params.py
@@ -1,4 +1,4 @@
-MAX_BLOCK_SIZE = 50 #Max of 50 transactions per block
+MAX_BLOCK_SIZE = 49 #Max of 50 transactions per block, including the coinbase
 BLOCK_REWARD = 50 #Lottery winner gets 50 BrowserCoins
 MAX_COINS = 42000000 #All the coins there will ever be - 42 million
 BLOCK_SPACING = 5 * 60 #5 minutes between blocks
