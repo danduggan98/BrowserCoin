45f73fe3f17007159f7b6e95647804737c50788a
= <dadu3890@colorado.edu>
2020-10-22 15:20:58 -0600

    Added route to accept transaction data

--- browsercoin/servers/node_server.py
+++ browsercoin/servers/node_server.py
@@ -1,8 +1,27 @@
 from browsercoin.src import node
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
+local_node = node.Node()
+
+@app.route('/node/transaction', methods=['POST'])
+def recieve_tx():
+   if not request.json:
+      return 'Request rejected - transaction data required'
+
+   amount    = request.json.get('amount')
+   sender    = request.json.get('sender')
+   recipient = request.json.get('recipient')
+   signature = request.json.get('signature')
+
+   if None in (amount, sender, recipient, signature):
+      return 'Request rejected - at least one parameter is missing'
+   
+   #Add tx to node
+   # -- Implementation -- #
+
+   return 'Request accepted - Transaction added to mempool'
 
 if __name__ == '__main__':
     print('Starting Node')
