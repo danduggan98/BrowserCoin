6d03bc4bc6c3d15826abe9e55499ca47e1f1808b
= <dadu3890@colorado.edu>
2020-10-21 19:21:44 -0600

    Basic prototype for transaction creation method

--- browsercoin/src/wallet.py
+++ browsercoin/src/wallet.py
@@ -1,3 +1,4 @@
+from browsercoin.src import blockchain
 import rsa
 import requests
 
@@ -6,8 +7,16 @@ class Wallet:
         self.addresses = []
     
     def create_new_address(self):
-        self.addresses.append(rsa.newkeys(512)[0])
+        self.addresses.append(rsa.newkeys(512))
+    
+    def create_transaction(self, amount, sender, recipient):
+        new_tx = blockchain.Transaction(amount, sender, recipient)
+        signed_tx = new_tx.sign() #where to get sk?
+        return signed_tx
     
     def broadcast_transaction(self, tx):
         # --- Implementation --- #
         return
+    
+    def find_sk(self, pk):
+        pass
