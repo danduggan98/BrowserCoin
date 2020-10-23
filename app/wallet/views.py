5aad62559a6d94583ac5abbd89bccd356635b1ac
= <dadu3890@colorado.edu>
2020-10-23 08:57:26 -0600

    Transaction form submission now pings node server

--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+import requests
+
+api_server = 'http://localhost:5000/node/transaction'
 
 # Create your views here.
 def wallet(request):
+    if req := request.POST:
+        print(req)
+        amount    = req['amount']
+        sender    = req['sender']
+        recipient = req['recipient']
+
+        response = requests.post(api_server)
+        print(response)
+    
+    return render(request, 'wallet.html')
+
+def process_tx(request):
+    print('Inside process_tx method')
+    
+    response = requests.post(api_server)
+
+    #return 'ACCESSED PROCESS_TX METHOD'
     return render(request, 'wallet.html')
