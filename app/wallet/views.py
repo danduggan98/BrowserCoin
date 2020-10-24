2becb53560dd3652cd548b9fbcda8a4010e77838
= <dadu3890@colorado.edu>
2020-10-24 10:07:43 -0600

    Display errors when transaction fields left empty

--- app/wallet/views.py
+++ app/wallet/views.py
@@ -1,28 +1,39 @@
 from django.shortcuts import render
+from browsercoin.src.blockchain import Transaction
 import requests
+import rsa
 
 api_server = 'http://localhost:5000/node/transaction'
+
+#CREATE KEYSET FOR TESTING ONLY
+
+(pk, sk) = rsa.newkeys(512)
 
 # Create your views here.
 def wallet(request):
     if req := request.POST:
         amount    = req['amount']
-        sender    = req['sender']
         recipient = req['recipient']
+        
+        errs = []
+
+        if not amount:
+            errs.append('Please enter the amount to send')
+            
+        if not recipient:
+            errs.append('Please enter the recipient')
+        
+        if errs:
+            return render(request, 'wallet.html', {'errs': errs})
+        
+        #CREATE TX + SIGN
 
         data = {
             'amount': amount,
-            'sender': sender,
+            'sender': pk,
             'recipient': recipient
         }
         response = requests.post(api_server, data=data)
-    
-    return render(request, 'wallet.html')
+        return render(request, 'wallet.html', {'errs': response})
 
-def process_tx(request):
-    print('Inside process_tx method')
-    
-    response = requests.post(api_server)
-
-    #return 'ACCESSED PROCESS_TX METHOD'
     return render(request, 'wallet.html')
