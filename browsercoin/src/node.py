ebd20c3c474c482f7c11b5e34ebdab9f5c16a46b
= <dadu3890@colorado.edu>
2020-10-24 20:43:24 -0600

    Store mempool in a deque rather than a Queue

--- browsercoin/src/node.py
+++ browsercoin/src/node.py
@@ -1,19 +1,19 @@
 from browsercoin.src import blockchain, crypto, params
-import queue
+from collections import deque
 import rsa
 
 class Node:
     def __init__(self):
-        self.mempool = queue.Queue()
+        self.mempool = deque()
         self.blockchain = blockchain.Blockchain()
         self.next_block_data = blockchain.BlockData()
 
     def include_transaction(self, tx: blockchain.Transaction):
-        self.mempool.put(tx)
+        self.mempool.append(tx)
     
     #Check the next transaction, and add it to the Block if valid
     def validate_next_transaction(self):
-        next_tx = self.mempool.get()
+        next_tx = self.mempool.pop()
 
         if (blockchain.transaction_is_valid(next_tx, self.next_block_data)):
             self.next_block_data.add_transaction(next_tx)
