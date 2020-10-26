0c8c45e127731896c2e827acae23a2c879dc7118
= <dadu3890@colorado.edu>
2020-10-26 10:34:20 -0600

    Added proper API routes

--- browsercoin/servers/test_server.py
+++ browsercoin/servers/test_server.py
@@ -1,54 +1,90 @@
-import datetime
-import threading
-import atexit
-from flask import Flask
+from browsercoin.src import blockchain, node
+from flask import Flask, request, Response
+import threading, atexit
+import json
+import jsonpickle
+import rsa
 
 # Multithreading implementation borrowed from this SO post:
 # https://stackoverflow.com/questions/14384739/how-can-i-add-a-background-thread-to-flask
 
-POOL_TIME = 1 #Seconds
-
-# variables that are accessible from anywhere
-commonDataStruct = []
-# lock to control access to variable
+POOL_TIME = 1 #1 second
 dataLock = threading.Lock()
-# thread handler
+
+local_node = node.Node()
 thread = threading.Thread()
 
+#Run the server
 def start_node():
     app = Flask(__name__)
 
-    @app.route('/test', methods=['GET'])
-    def recieve_test():
-        print('--- API REQUEST RECIEVED ---')
-        return 'Nice'
+    # //////// API Handlers \\\\\\\\ #
+    @app.route('/node/transaction', methods=['POST'])
+    def recieve_tx():
+        if not request.json:
+            return Response('Request rejected - transaction data required', status=400, mimetype='application/json')
 
+        tx: blockchain.Transaction = jsonpickle.decode(request.json)
+
+        if None in (tx.timestamp, tx.transfer_amount, tx.sender, tx.recipient, tx.hash):
+            return Response('Request rejected - at least one parameter is missing', status=400, mimetype='application/json')
+        
+        if tx.signature is None:
+            return Response('Request rejected - signature is missing or invalid', status=400, mimetype='application/json')
+        
+        #Add transaction to node
+        tx.sender_prev_tx    = local_node.blockchain.latest_address_activity(tx.sender)
+        tx.recipient_prev_tx = local_node.blockchain.latest_address_activity(tx.recipient)
+
+        local_node.include_transaction(tx)
+        return Response('Request accepted - Transaction added to mempool', status=202, mimetype='application/json')
+    
+    # //////// Test routes for checking results (TEMPORARY) \\\\\\\\ #
+    @app.route('/node/info', methods=['GET'])
+    def info():
+        return str(local_node.blockchain)
+
+    @app.route('/node/mempool', methods=['GET'])
+    def mempool():
+        result = ''
+        for tx in local_node.mempool:
+            result += str(tx)
+            result += '\n'
+        return result
+
+    @app.route('/node/valid', methods=['GET'])
+    def valid():
+        return str(local_node.mempool[0].is_valid())
+
+    @app.route('/node/block', methods=['GET'])
+    def block():
+        return str(local_node.next_block_data)
+
+    # //////// Methods for multithreaded transaction processing \\\\\\\\ #
     def stop_processing():
         global thread
         thread.cancel()
 
     def process_tx():
-        global commonDataStruct
+        global local_node
         global thread
-        with dataLock:
-        # Do your stuff with commonDataStruct Here
-            print('! ! ! DOING THE NEXT THING ! ! !', datetime.datetime.now())
 
-        # Set the next thread to happen
+        with dataLock:
+        # Validate the transaction
+            #TODO
+            print('! ! ! DOING THE NEXT THING ! ! !')
+
         thread = threading.Timer(POOL_TIME, process_tx, ())
         thread.start()   
 
     def begin_processing():
-        # Do initialisation stuff here
         global thread
-        # Create your thread
         thread = threading.Timer(POOL_TIME, process_tx, ())
         thread.start()
 
-    # Initiate
+    #Start handling transactions
     begin_processing()
-    # When you kill Flask (SIGTERM), clear the trigger for the next thread
-    atexit.register(stop_processing)
+    atexit.register(stop_processing) #Stop processing when server ends
     return app.run(debug=True, use_reloader=False)
 
 app = start_node()
