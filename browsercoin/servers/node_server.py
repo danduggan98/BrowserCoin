6e2adf411c8c5b086f62bb76eb52d594d1c0e2c9
= <dadu3890@colorado.edu>
2020-10-27 15:28:56 -0600

    Moved block selection loop to masternode server

--- browsercoin/servers/node_server.py
+++ browsercoin/servers/node_server.py
@@ -8,14 +8,12 @@ import rsa
 # Multithreading implementation borrowed from this SO post:
 # https://stackoverflow.com/questions/14384739/how-can-i-add-a-background-thread-to-flask
 
-POOL_TIME = 1 #Process one tx every second
-BLOCK_TIME = params.BLOCK_SPACING
+PROCESSING_TIME = 1 #Process one tx every second
 dataLock = threading.Lock()
 
 #Create Node, threads for transaction processing and adding blocks
 local_node = node.Node()
 thread = threading.Thread()
-block_thread = threading.Thread()
 
 #Run the server
 def start_node():
@@ -24,21 +22,12 @@ def start_node():
     # //////// API Handlers \\\\\\\\ #
     @app.route('/node/transaction', methods=['POST'])
     def recieve_tx():
-        if not request.json:
-            return Response('Request rejected - transaction data required', status=400, mimetype='application/json')
 
-        tx: blockchain.Transaction = jsonpickle.decode(request.json)
-
-        if None in (tx.timestamp, tx.transfer_amount, tx.sender, tx.recipient, tx.hash):
-            return Response('Request rejected - at least one parameter is missing', status=400, mimetype='application/json')
-        
-        if tx.signature is None:
-            return Response('Request rejected - signature is missing or invalid', status=400, mimetype='application/json')
-        
         #Add transaction to node
-        tx.sender_prev_tx    = local_node.blockchain.latest_address_activity(tx.sender)
+        tx: blockchain.Transaction = jsonpickle.decode(request.json)
+        tx.sender_prev_tx = local_node.blockchain.latest_address_activity(tx.sender)
         tx.recipient_prev_tx = local_node.blockchain.latest_address_activity(tx.recipient)
-
+        
         local_node.include_transaction(tx)
         return Response('Request accepted - Transaction added to mempool', status=202, mimetype='application/json')
     
@@ -79,31 +68,14 @@ def start_node():
         with dataLock:
             local_node.validate_next_transaction()
 
-        thread = threading.Timer(POOL_TIME, process_tx, ())
+        thread = threading.Timer(PROCESSING_TIME, process_tx, ())
         thread.start()
-    
-    def add_block():
-        global local_node
-        global block_thread
-
-        with dataLock:
-            local_node.add_next_block()
-
-        block_thread = threading.Timer(BLOCK_TIME, add_block, ())
-        block_thread.start()
 
     def begin_processing():
-        begin_adding_blocks()
-
         global thread
-        thread = threading.Timer(POOL_TIME, process_tx, ())
+        thread = threading.Timer(PROCESSING_TIME, process_tx, ())
         thread.start()
     
-    def begin_adding_blocks():
-        global block_thread
-        block_thread = threading.Timer(BLOCK_TIME, add_block, ())
-        block_thread.start()
-
     #Start handling transactions
     begin_processing()
     atexit.register(stop_processing) #Stop processing when server ends
