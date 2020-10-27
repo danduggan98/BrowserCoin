a44d3d087b3ebed7a7a362bb392dae9d4900e161
= <dadu3890@colorado.edu>
2020-10-27 11:11:46 -0600

    Proper description for block reward

--- browsercoin/src/params.py
+++ browsercoin/src/params.py
@@ -1,5 +1,5 @@
 MAX_BLOCK_SIZE = 49 #Max of 50 transactions per block, including the coinbase
-BLOCK_REWARD = 100 #Lottery winner gets 50 BrowserCoins
+BLOCK_REWARD = 100 #Lottery winner gets 100 BrowserCoins
 MAX_COINS = 42000000 #All the coins there will ever be - 42 million
 BLOCK_SPACING = 3 * 60 #3 minutes between blocks
 
