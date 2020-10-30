784040c866aa5e4db4f0b0aa35d58a1006bd8d51
= <dadu3890@colorado.edu>
2020-10-30 04:43:43 -0600

    Increased block spacing to 5 minutes

--- browsercoin/src/params.py
+++ browsercoin/src/params.py
@@ -1,7 +1,7 @@
 MAX_BLOCK_SIZE = 49 #Max of 50 transactions per block, including the coinbase
 BLOCK_REWARD = 100 #Lottery winner gets 100 BrowserCoins
 MAX_COINS = 42000000 #All the coins there will ever be - 42 million
-BLOCK_SPACING = 3 * 60 #3 minutes between blocks
+BLOCK_SPACING = 5 * 60 #5 minutes between blocks
 
 #Public key used as sender for all coinbase transactions
 import rsa
