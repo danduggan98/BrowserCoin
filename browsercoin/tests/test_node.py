09b3bdeb92b22aff2083b7b3b827f9d8d60c7405
= <dadu3890@colorado.edu>
2020-11-03 12:02:23 -0700

    Added a test for empty block validity

--- browsercoin/tests/test_node.py
+++ browsercoin/tests/test_node.py
@@ -10,10 +10,13 @@ def test_node():
     chain = blockchain.Blockchain()
     
     block1data = BlockData()
-    master.add_coinbase(testpub, block1data)
+    empty_block = Block(block1data)
+    assert not chain.block_is_valid(empty_block)
 
+    master.add_coinbase(testpub, block1data)
     new_block = Block(block1data)
     assert chain.block_is_valid(new_block)
+    
     chain.add_block(new_block)
     assert chain.get_balance(testpub) == 100
     assert chain.nth_block(1).is_valid()
